07130bbf2240b4ca43b748a9c4cc090c9cf6ea6e
Silas Dias <silabao47@gmail.com>
2022-10-18 14:48:01 -0300

    first commit

---
+++ src/praticandojava/PraticandoJava.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praticandojava;
+
+/**
+ *
+ * @author silab
+ */
+public class PraticandoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //System.out.println("Eae Silas");
+       //System.out.println("o codigo a baixo é apenas para apresentar no visualizador qq coisa");
+       //System.out.println("vou fazer uma conta matematica");
+       //System.out.println("2+2=" + 2+2 );
+       //Viu isso aqui é uma palavra pq está  " aqui dentro "
+       // a pausa na programaçao seria isso aqui, que é chamado de comentarios
+       // todo comentario começa com // ou /**/
+       // no caso do /**/ tem que ta em volta
+      // é igual 
+      // por enquanto nao foca no parentes 
+      
+      //blz
+ // vamos comecar apartir daqui,               
+ // eu usei o exemplo de cima pra te deixar curioso
+ 
+ /*
+        Vamos ver uma coisa chamada Tipos Primitivos
+        Memoriza ai TIPOS PRIMITIVOS o que são ??
+        na linguagem de maquina tudo é 0 e 1 e a partir disso se constroi qq coisa
+        o que difere 2 de valor numerico pra 2 de caractere ??agora vc me pegou
+        boa
+        
+ 
+ 
+ */
+        int valorNumerico = 2;
+        String valorCaractere = "2";
+        boolean valorFalseOuTrue = true;
+        double numeroDecimal = 2.2;
+        
+        
+        //System.out.println(valorNumerico );
+        //System.out.println(valorCaractere );
+        //System.out.println(valorFalseOuTrue );
+        //System.out.println(numeroDecimal );
+        
+        //olha que interessante valor numerico é 2 isso se chama variaveis 
+        //e sao primitivas
+        
+        System.out.println(valorNumerico + valorNumerico );
+        System.out.println(valorCaractere+valorCaractere);
+    }
+    
+}
